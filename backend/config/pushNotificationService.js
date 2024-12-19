0c5f2ef049bfce21cad1772d78858955b9c0653f
rogelio <andresrogesu@gmail.com>
2024-12-19 09:45:25 -0300

    Update pushNotificationService.js

--- backend/config/pushNotificationService.js
+++ backend/config/pushNotificationService.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Asegúrate de cargar las variables de entorno
 
-const serviceAccountJson = Buffer.from(process.env.GOOGLE_SERVICE_ACCOUNT_KEY, 'base64').toString('utf-8');
+
+const base64Credentials = process.env.GOOGLE_CREDENTIALS.replace(/\n/g, '');
+const serviceAccountJson = Buffer.from(base64Credentials, 'base64').toString('utf-8');
 const serviceAccount = JSON.parse(serviceAccountJson);
 
 admin.initializeApp({
