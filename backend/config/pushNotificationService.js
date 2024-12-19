9e6ce7c29c539afdab395ff664a2efda9934157d
Rogelio Suleta <andresrogesu@gmail.com>
2024-12-19 08:58:08 -0300

    ..

--- backend/config/pushNotificationService.js
+++ backend/config/pushNotificationService.js
@@ -3,18 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Asegúrate de cargar las variables de entorno
 
-const serviceAccount = {
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Necesario para manejar correctamente los saltos de línea
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
-};
+const serviceAccountJson = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_BASE64, 'base64').toString('utf-8');
+const serviceAccount = JSON.parse(serviceAccountJson);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
