1c0ad2204afd122af9660281813d5381136652dc
Rogelio Suleta <andresrogesu@gmail.com>
2025-01-19 14:36:50 -0300

    ..

--- backend/controllers/Tasks.controller.js
+++ backend/controllers/Tasks.controller.js
@@ -45,11 +45,6 @@ export const createTask = async (req, res) => {
                 'data.deviceToken': deviceToken,
                 'data.title': title,
             });
-            const job1= await agenda.schedule('sendTaskNotification', {
-                deviceToken,
-                title,
-            });
-            console.log("job1", job1)
 
             if (existingJob.length === 0) {
                 const job = await agenda.schedule(notificationTime, 'sendTaskNotification', {
